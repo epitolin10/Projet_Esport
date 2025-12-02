c550078ddaa2799a5a0fd18b68b1a97ccfb81755
epitolin10 <pitolinenzo10@gmail.com>
2025-12-02 14:35:29 +0100

    Mise à jour de la migration équipe

--- database/migrations/2025_11_14_113800_create_equipes_table.php
+++ database/migrations/2025_11_14_113800_create_equipes_table.php
@@ -6,25 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('equipes', function (Blueprint $table) {
             $table->id();
-            // Le nom equipe peut être nulable si le joueur n'appartient à aucune équipe
-            $table->string('nom_equipe')->nullable();
+            $table->string('nom_equipe');
             $table->string('tag')->nullable();
-            $table->string('Pays')->nullable();
-            $table->text('logo_url')->nullable();
+            $table->string('pays')->nullable(); // <-- s'assurer que cette colonne existe
+            $table->string('logo_url')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('equipes');
